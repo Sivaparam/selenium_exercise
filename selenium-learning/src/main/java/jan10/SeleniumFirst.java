c7f2c2f6d89a84bde9ebde2e2b1c56af4a440aee
ranjani <shiva.ranjani83@gmail.com>
2023-01-10 20:28:49 -0500

    maven project for selenium

---
+++ selenium-learning/src/main/java/jan10/SeleniumFirst.java
@@ -0,0 +1,15 @@
+package jan10;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumFirst{
+	
+	
+	public static void main(String args[]) {
+		System.out.print("Hi");
+		
+	    WebDriver driver = new ChromeDriver();
+	    
+
+	}
+}
