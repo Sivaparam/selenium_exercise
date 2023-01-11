fa8d9214ece06dce892497a0006ab48b22bbe68e
ranjani <shiva.ranjani83@gmail.com>
2023-01-11 16:55:25 -0500

    Webdriver commands

--- selenium-learning/src/main/java/jan10/SeleniumFirst.java
+++ selenium-learning/src/main/java/jan10/SeleniumFirst.java
@@ -1,15 +1,19 @@
 package jan10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumFirst{
-	
-	
+public class SeleniumFirst {
+
 	public static void main(String args[]) {
 		System.out.print("Hi");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://www.macys.com/");
+		driver.findElement(By.id("globalSearchInputField")).sendKeys("Levis Jeans");
+		driver.findElement(By.id("globalSearchInputField")).sendKeys(Keys.ENTER);
 		
-	    WebDriver driver = new ChromeDriver();
-	    
 
 	}
 }
---
+++ selenium-learning/src/main/java/jan11/TitleTest.java
@@ -0,0 +1,33 @@
+package jan11;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TitleTest {
+
+	public static void main(String[] args) {
+		
+			WebDriver driver = new ChromeDriver();
+				
+			String title = "Amazon.com. Spend less. Smile more1";
+				
+			//launch chrome and driver to url provided
+			driver.get("https://www.amazon.com/");
+			
+			//get the actual title
+			String actualTitle = driver.getTitle();
+				
+			//compare the titles
+			if(actualTitle.equalsIgnoreCase(title)) {
+				System.out.println("Title test passed" + actualTitle + " : " + title);
+			} else {
+				System.out.println("Title test failed" + actualTitle + " : " + title);
+			}
+			
+				
+			//close browser
+			driver.close();
+				
+		}
+	}
+
