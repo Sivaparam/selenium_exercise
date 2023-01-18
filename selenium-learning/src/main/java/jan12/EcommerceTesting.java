0b0085a29f6c085272a68dfa7f16818f6dac802c
ranjani <shiva.ranjani83@gmail.com>
2023-01-18 15:30:18 -0500

    ecommerce

--- selenium-learning/src/main/java/jan12/EcommerceTesting.java
+++ selenium-learning/src/main/java/jan12/EcommerceTesting.java
@@ -17,10 +17,11 @@ public class EcommerceTesting {
 	WebDriver driver = new ChromeDriver();
 	
 	//method to launch browser
-	public void launchBrowser() {
+	public void launchBrowser() throws InterruptedException {
 		//navigate to gap.com home page
 		driver.get("https://www.gap.com/");	
 		driver.manage().window().maximize();
+		Thread.sleep(2000);
 		WebElement ele = driver.findElement(By.className("sitewide-9pwq9q"));
 		if(ele != null)
 			ele.click();
@@ -31,7 +32,7 @@ public class EcommerceTesting {
 	
 	public void searchProduct() throws InterruptedException {
 		
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		
 		//search for product and press enter
 		driver.findElement(By.name("searchText")).sendKeys("Womens Dress");
@@ -41,12 +42,23 @@ public class EcommerceTesting {
 	
 	public void addToCart() throws InterruptedException {
 		
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		
 	//get list of items displayed and iterator for product
 		List<WebElement> allProducts = driver.findElements(By.className("css-1ohx80u"));
-		WebElement prod = allProducts.get(1);
+		//get the select items from list (index starts with 0)
+		WebElement prod = allProducts.get(0);
+		//click on the item
 		prod.click();
+		
+		//select size
+		
+		Thread.sleep(3000);
+		driver.findElement(By.className("promoDrawer__handlebar__icon")).click();
+		driver.findElement(By.id("variant-1-sizeDimension1-L")).click();
+		
+		//click add to bag
+		driver.findElement(By.id("AddToBagFromBuyBox_add-to-bag__button")).click();
 		
 		
 // quick add to cart 
@@ -59,7 +71,21 @@ public class EcommerceTesting {
 		
 	}
 	
-	public void checkOut() {
+	public void checkOut() throws InterruptedException {
+		
+		//click on checkout button from the pop up
+		Thread.sleep(2000);
+		driver.findElement(By.id("checkoutButton")).click();
+		
+		//click on checkout button from the checkout page
+		driver.findElement(By.id("checkout-button")).click();
+		
+		//click on continue as guest
+		List<WebElement> buttons = driver.findElements(By.tagName("button"));
+		buttons.get(1).click();
+		//enter the email address
+		driver.findElement(By.id("verify-account-email")).sendKeys("abc@gmail.com");
+		driver.findElement(By.className("loyalty-guest-signin-button cb_fixed-button-primary css-0")).click();
 		
 	}
 	
